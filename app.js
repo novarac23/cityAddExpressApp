81f5b4a7cab44a10247c84cb038ccb6bd7543b16
Nikola Novakovic <nikola.novakovic9@gmail.com>
2015-07-14 20:54:15 +0200

    Listing cities on index.html

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('OK');
+app.use(express.static('public'));
+
+app.get('/cities', function(req, res) {
+  var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+  res.json(cities);
 });
 
 module.exports = app;
