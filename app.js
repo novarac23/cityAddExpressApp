8cb893355e647c3cab53d7a8b707554604a53ae5
Nikola Novakovic <nikola.novakovic9@gmail.com>
2015-07-15 23:13:35 +0200

    No select for production

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ if (process.env.REDISTOGO_URL) {
   client.auth(rtg.auth.split(":")[1]);
 } else {
   var client = redis.createClient();
+  client.select((process.env.NODE_ENV || 'development').length);
 }
 
-client.select((process.env.NODE_ENV || 'development').length);
 //End redis connection
 
 app.get('/cities', function(req, res) {
